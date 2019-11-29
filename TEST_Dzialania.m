cc50a830c8aa36681a6c2d119b32d3a987517129
DTGT <themegatgt48@gmail.com>
2019-11-29 22:24:49 +0100

    Pierwszy Szkic Sprawozdania, aktualizacja ToDo i drobna poprawka w TEST_Dzialania.m

--- TEST_Dzialania.m
+++ TEST_Dzialania.m
@@ -2,8 +2,7 @@ function x = TEST_Dzialania(A,b)
 % TEST_Dzialania Przyjmuje macierz A oraz wektor b
     % i rozwiazuje A*x=b
 
-p = size(A);
-[L, U] = CroutPartition(A, p(1)/3);
+[L, U] = CroutPartition(A);
 y = blok3L(L, b);
 x = blok3U(U, y);
 
